4a0c492ae9ad242246ad78a3af7e0d19aac8428e
Gouranga123456789 <gourangakalita17@gmail.com>
2025-11-23 10:33:19 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const mongoURI = "mongodb+srv://gourangakalita17_db_user:YMVVswd4DJbEt15F@cluster0.ritfqm3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
